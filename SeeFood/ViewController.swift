475b5bd470cc52b9649f50ac6ddecf1a72831b7b
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2020-10-05 19:09:01 +0900

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by 1101373 on 2020/10/05.
+//
+
+import CoreML
+import UIKit
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
